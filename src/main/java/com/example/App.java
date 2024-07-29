18dbf7495821a846ebfb48f7ae2830c18bc14ef5
Arshad Khan <62833447+arshadkhan99637@users.noreply.github.com>
2024-07-29 16:14:46 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Good evening World!" );
+         System.out.println( "Have a nice day" );
     }
 }
